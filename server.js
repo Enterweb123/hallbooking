49fc0f8bb5c1b70ce963f6bf59e2a73a87d1f396
Mohan <97966135+Enterweb123@users.noreply.github.com>
2023-02-04 19:51:03 +0530

    hall booing api

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+
+const bodyparser = require("body-parser");
+const connectDB = require("./config/db");
+const uniqid = require("uniqid");
+const apiRouter = require("./routes");
+
+const app = express();
+const port = process.env.PORT || 4000 ;
+app.use(bodyparser.json());
+
+connectDB();
+app.use(express.json());
+
+app.use("/api",apiRouter)
+
+app.get("/",(req,res)=>{
+ res.send("its working ")
+})
+app.listen(port,()=>{
+    console.log(`server is up and running on port ${port}`);
+})
